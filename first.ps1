361f9e8d9ee0d10fab79b6a0e3bb730a07c1c478
Alexander Sinno <alex.sinno@gmail.com>
2022-06-24 16:02:05 +0200

    Update first.ps1

--- first.ps1
+++ first.ps1
@@ -8,7 +8,7 @@ $TimeCounter = 0
 $SuspiciousTicket = @()
 
 #Attempts to match all times in klist and dumps them into a psobject for collection
-foreach($Line in $LIST | Select-String  -Pattern '(Start Time)|(End Time)') {
+$result2 = foreach($Line in $LIST | Select-String  -Pattern '(Start Time)|(End Time)') {
   $Line -match $Regex
   $StartDateTime = [DateTime]::ParseExact($Matches[0],$DateTimeFormat,$null)
   $TimeMatches += $StartDateTime
@@ -21,4 +21,4 @@ $result = for($TicketCount -gt 0; $TimeCounter -lt $TicketCount; $TimeCounter+=2
     #Creates object to store ticket and device properties to be sent over UDP or any other protocol the user would like.
 'This alert has detected a suspicious ticket on an endpoint. It works by checking if the expiration time of a ticket is greater than 10-hours. This typically indicates the presence of a golden ticket or silver ticket.';
       }else {'No suspicious TGT'}
-} $result | Out-File $env:TEMP\tgt.txt
+} $result + $result2 | Out-File $env:TEMP\tgt.txt
